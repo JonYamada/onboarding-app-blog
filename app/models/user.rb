2d6e775761664859843cf8179f610669928bf172
yamadaj4 <jonathon.yamada@wpengine.com>
2022-05-04 12:59:30 +0100

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  validates :first_name, :email, presence: true
+  validates_uniqueness_of :email
+
+  EMAIL_REGEX = /^(.+)@(.+)$/
+
+  def valid_email?(email)
+    email =~ EMAIL_REGEX
+  end
+
+  # p valid_email?('john.doe@domain.com' ? 'Valid Email' ∶ 'Invalid Email')
 end
