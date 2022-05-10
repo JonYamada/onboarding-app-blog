7aed0d44064be9b084abec7e720687809c2dca59
yamadaj4 <jonathon.yamada@wpengine.com>
2022-05-10 14:34:51 +0100

    created articles controller

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,14 @@
+class ArticlesController < ApplicationController
+  layout 'hello_world'
+  helper_method :initials
+
+  def index; end
+
+  def new; end
+
+  def create; end
+
+  def slug
+
+  end
+end
