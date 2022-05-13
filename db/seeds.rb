501576006a78b9dd1d99c01b4921d27ce11cc527
yamadaj4 <jonathon.yamada@wpengine.com>
2022-05-13 12:44:04 +0100

    added seed file to have 1 user and 7 articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 p 'Seeding Users...'
-User.create(first_name: 'jon', last_name: 'yams', email: 'jonathon.yamada@wpengine.com')
+User.find_or_create_by(first_name: 'jon', last_name: 'yams', email: 'jonathon.yamada@wpengine.com')
 
 p 'Seeding Articles...'
 7.times do |index|
-  Article.create(
+  Article.find_or_create_by(
     title: "Title #{index}",
     content: "Lorem ipsum dolor sit amet. Qui expedita sapiente aut minima nesciunt et voluptas laboriosam ea voluptatibus nobis et possimus consequatur. Id amet omnis qui cupiditate quasi rem distinctio voluptatem qui galisum deserunt qui consectetur alias At expedita exercitationem cum nulla voluptas. Et doloribus repellat et nesciunt similique ut tenetur neque et reprehenderit quas ut quaerat ullam!",
     user_id: User.find_by_email('jonathon.yamada@wpengine.com').id
