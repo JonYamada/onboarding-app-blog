7f5b57684e7a71067fd438be48ab260d67a92f74
yamadaj4 <jonathon.yamada@wpengine.com>
2022-05-17 15:54:46 +0100

    extracted submit function to "new" action component

--- app/javascript/bundles/views/articles/new.tsx
+++ app/javascript/bundles/views/articles/new.tsx
@@ -1,20 +1,36 @@
-import React from 'react';
-import {string} from 'prop-types';
+import React, {useState} from 'react'
 import ArticleForm from './common/form'
 import {IParams} from './interfaces'
+import {createArticle} from '../../api/articles/articles'
+import {redirectTo} from '../../utils/nav'
+import {toast as toastTranslations} from '../../config/translations/en.json'
+import {toast} from 'react-hot-toast'
 
 const defaultProps = {
   className: null
 }
 
-const NewArticle = ({article, className}: { article: IParams, className: string }) => {
-  return (
-    <div className={`new-article ${className}`}>
-      <ArticleForm article={article}/>
-    </div>
-  );
-};
+const NewArticle = ({className}: { article: IParams, className: string }) => {
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = (values: IParams) => {
+    setLoading(true)
+    createArticle(values)
+      .then(({request}) => {
+        redirectTo(request?.responseURL, {
+          toast: {
+            message: toastTranslations.successSaved,
+            type: 'success',
+          }
+        })
+      })
+      .catch(() => toast.error(toastTranslations.errorGeneric))
+      .finally(() => setLoading(false))
+  }
+
+  return <ArticleForm onSubmit={handleSubmit} className={`new-article ${className}`}/>
+}
 
 NewArticle.defaultProps = defaultProps
 
-export default NewArticle;
+export default NewArticle
