2c0291e4faee8991d15bdc0987f3320929839497
yamadaj4 <jonathon.yamada@wpengine.com>
2022-05-19 11:00:02 +0100

    temporarily hard coded user_id in POST request

--- app/javascript/bundles/api/articles/articles.ts
+++ app/javascript/bundles/api/articles/articles.ts
@@ -2,6 +2,8 @@ import {IParams} from '../../views/articles/interfaces'
 import {client} from '../setup'
 
 const createArticle = (params: IParams) => {
+  // TODO add to current user when available
+  params.user_id = 1
   return new Promise(((resolve, reject) => {
     client.post('/articles', {article: params})
       .then(resolve)
