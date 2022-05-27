bed62eb79198d299a5aa779a8c9d3ab9235a9754
yamadaj4 <jonathon.yamada@wpengine.com>
2022-05-27 11:08:35 +0100

    added article controller and model spec files

---
+++ spec/requests/article_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "Articles", type: :request do
+  describe "GET /index" do
+    it 'retrieves articles' do
+      # TODO: complete this test
+      skip
+    end
+
+    it 'renders the index template' do
+      # TODO: complete this test
+      skip
+    end
+  end
+
+  describe "POST /create" do
+
+
+  end
+end
