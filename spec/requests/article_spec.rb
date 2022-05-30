fe5d5b8195a3a6dcc868fb38299d545da515dc5b
yamadaj4 <jonathon.yamada@wpengine.com>
2022-05-30 12:20:20 +0100

    user hooks to make user an instance variable

--- spec/requests/article_spec.rb
+++ spec/requests/article_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Articles", type: :request do
+  before(:context) do
+    @user = User.find_or_create_by!(first_name: 'jon', last_name: 'yams', email: 'jonathon.yamada@wpengine.com')
+  end
+
+  after(:context) do
+    Article.all.destroy_all unless Article.count.zero?
+  end
+
   describe "GET /index" do
     skip 'retrieves articles' do
       # TODO: complete this test
@@ -13,10 +21,9 @@ RSpec.describe "Articles", type: :request do
 
   describe 'POST /create' do
     it 'successfully creates an article' do
-      user = User.find_or_create_by!(first_name: 'jon', last_name: 'yams', email: 'jonathon.yamada@wpengine.com')
       initial_article_count = Article.count
 
-      post '/articles', params: { article: { title: 'Test Title', content: 'Test Content', user_id: user.id } }
+      post '/articles', params: { article: { title: 'Test Title', content: 'Test Content', user_id: @user.id } }
 
       expect(response).to redirect_to(:articles)
       expect(response).to have_http_status(301)
@@ -24,11 +31,10 @@ RSpec.describe "Articles", type: :request do
     end
 
     it 'gracefully handles save errors' do
-      user = User.find_or_create_by!(first_name: 'jon', last_name: 'yams', email: 'jonathon.yamada@wpengine.com')
       initial_article_count = Article.count
 
-      params_blank_content = { article: { title: 'Test Title', content: '', user_id: user.id } }
-      params_blank_title = { article: { title: '', content: 'Test Contents', user_id: user.id } }
+      params_blank_content = { article: { title: 'Test Title', content: '', user_id: @user.id } }
+      params_blank_title = { article: { title: '', content: 'Test Contents', user_id: @user.id } }
       params_blank_user_id = { article: { title: 'Test Title', content: 'Test Contents', user_id: nil } }
 
       [params_blank_title, params_blank_content, params_blank_user_id].each do |params|
