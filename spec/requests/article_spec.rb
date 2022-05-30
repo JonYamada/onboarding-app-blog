699d1cdbb9e16987ee59553ddd142e376a119581
yamadaj4 <jonathon.yamada@wpengine.com>
2022-05-30 11:44:33 +0100

    added rspec tests for create method

--- spec/requests/article_spec.rb
+++ spec/requests/article_spec.rb
@@ -2,19 +2,40 @@ require 'rails_helper'
 
 RSpec.describe "Articles", type: :request do
   describe "GET /index" do
-    it 'retrieves articles' do
+    skip 'retrieves articles' do
       # TODO: complete this test
-      skip
     end
 
-    it 'renders the index template' do
+    skip 'renders the index template' do
       # TODO: complete this test
-      skip
     end
   end
 
-  describe "POST /create" do
+  describe 'POST /create' do
+    it 'successfully creates an article' do
+      user = User.find_or_create_by!(first_name: 'jon', last_name: 'yams', email: 'jonathon.yamada@wpengine.com')
+      initial_article_count = Article.count
 
+      post '/articles', params: { article: { title: 'Test Title', content: 'Test Content', user_id: user.id } }
 
+      expect(response).to redirect_to(:articles)
+      expect(response).to have_http_status(301)
+      expect(Article.count).eql?(initial_article_count + 1)
+    end
+
+    it 'gracefully handles save errors' do
+      user = User.find_or_create_by!(first_name: 'jon', last_name: 'yams', email: 'jonathon.yamada@wpengine.com')
+      initial_article_count = Article.count
+
+      params_blank_content = { article: { title: 'Test Title', content: '', user_id: user.id } }
+      params_blank_title = { article: { title: '', content: 'Test Contents', user_id: user.id } }
+      params_blank_user_id = { article: { title: 'Test Title', content: 'Test Contents', user_id: nil } }
+
+      [params_blank_title, params_blank_content, params_blank_user_id].each do |params|
+        post '/articles', params: params
+        expect(response).to have_http_status(422)
+        expect(initial_article_count).eql?(Article.count)
+      end
+    end
   end
 end
