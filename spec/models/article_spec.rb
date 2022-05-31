b65b173064779f865c77fb4089e86f22bc63cd8d
yamadaj4 <jonathon.yamada@wpengine.com>
2022-05-31 10:53:28 +0100

    added model unit test and debugger

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:context) do
+    @user = User.find_or_create_by!(first_name: 'jon', last_name: 'yams', email: 'jonathon.yamada@wpengine.com')
+  end
+
+  after(:context) do
+    Article.destroy_all unless Article.count.zero?
+  end
+
+  it 'should have a required title field' do
+    article = Article.create(title: '', content: 'contents', user_id: @user.id)
+    p Article.all
+    expect(article).to_not be_valid
+    expect(article.errors.messages[:title].count).to be > 0
+    expect(article.errors.messages[:title][0]).to eq("can't be blank")
+  end
+
+  it 'should have a required content field' do
+    article = Article.create(title: 'title', content: '', user_id: @user.id)
+    expect(article).to_not be_valid
+    expect(article.errors.messages[:content].count).to be > 0
+    expect(article.errors.messages[:content][0]).to eq("can't be blank")
+  end
 end
