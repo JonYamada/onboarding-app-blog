951b744351786421668d373ed6e5d0876bfed9c9
JonYamada <jonathon.yamada@wpengine.com>
2022-06-17 12:35:26 +0100

    removed todo to JIRA ticket 2564

--- app/javascript/bundles/api/articles/articles.ts
+++ app/javascript/bundles/api/articles/articles.ts
@@ -3,7 +3,6 @@ import {client} from '../setup'
 import {ARTICLES_PATH} from '../../constants/routes'
 
 const createArticle = (params: IParams) => {
-  // TODO add to current user when available
   params.user_id = 1
   return new Promise(((resolve, reject) => {
     client.post(ARTICLES_PATH, {article: params})
