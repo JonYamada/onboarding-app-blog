a76fea6a4ba7590ddee4fe88d11a985a62ca0782
JonYamada <jonathon.yamada@wpengine.com>
2022-06-17 12:27:12 +0100

    removed card wrapper component

--- app/javascript/bundles/views/articles/common/form.tsx
+++ app/javascript/bundles/views/articles/common/form.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
-import Card from '../../../components/card/Card'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
 import LoadingButton from '@mui/lab/LoadingButton'
 import RichTextEditor from '../../../components/editor/RichTextEditor'
 import {Box, Input} from '@mui/material'
@@ -52,28 +53,30 @@ const ArticleForm = ({article, className, loading, onSubmit: onSave}: IArticleFo
         {({errors: formikErrors}) => (
           <Form>
             <Card>
-              <Box sx={{mb: 1}}>
-                <label htmlFor='title'>{form.label.title}</label>
-                <Input
-                  type='text'
-                  error={!!formikErrors?.title}
-                  id='title'
-                  name='title'
-                  onChange={({target: {value: title}}) => setValues({...values, title})}
-                  sx={{width: '100%', marginBottom: 1}}
-                />
-                <ErrorMessage className='error-message' name='title' component='div'/>
-              </Box>
-              <Box sx={{mb: 1}}>
-                <label htmlFor='content'>{form.label.content}</label>
-                <RichTextEditor
-                  ariaLabel='aria-editor'
-                  className={`${!!formikErrors?.content && 'border-invalid'}`}
-                  onChange={html => setValues({...values, content: html})}
-                  onError={({isEmpty, message}) => setErrors({...errors, content: {isEmpty, message}})}
-                />
-                <ErrorMessage className='error-message' name='content' component='div'/>
-              </Box>
+              <CardContent>
+                <Box sx={{mb: 1}}>
+                  <label htmlFor='title'>{form.label.title}</label>
+                  <Input
+                    type='text'
+                    error={!!formikErrors?.title}
+                    id='title'
+                    name='title'
+                    onChange={({target: {value: title}}) => setValues({...values, title})}
+                    sx={{width: '100%', marginBottom: 1}}
+                  />
+                  <ErrorMessage className='error-message' name='title' component='div'/>
+                </Box>
+                <Box sx={{mb: 1}}>
+                  <label htmlFor='content'>{form.label.content}</label>
+                  <RichTextEditor
+                    ariaLabel='aria-editor'
+                    className={`${!!formikErrors?.content && 'border-invalid'}`}
+                    onChange={html => setValues({...values, content: html})}
+                    onError={({isEmpty, message}) => setErrors({...errors, content: {isEmpty, message}})}
+                  />
+                  <ErrorMessage className='error-message' name='content' component='div'/>
+                </Box>
+              </CardContent>
             </Card>
 
             <LoadingButton
