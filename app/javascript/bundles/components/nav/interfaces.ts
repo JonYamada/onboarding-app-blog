89f0d04befbff8a200b60e4eda85fd16dfdf7f99
JonYamada <jonathon.yamada@wpengine.com>
2022-06-22 11:42:23 +0100

    added prettier plugin to Rubymine

--- app/javascript/bundles/components/nav/interfaces.ts
+++ app/javascript/bundles/components/nav/interfaces.ts
@@ -1,13 +1,13 @@
-import {ReactNode} from 'react'
-import {AppBarProps as MuiAppBarProps} from '@mui/material/AppBar/AppBar'
+import { ReactNode } from "react";
+import { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar/AppBar";
 
 interface IAppBarProps extends MuiAppBarProps {
   open?: boolean;
 }
 
 interface ISideNavProps {
-  children: ReactNode
-  width: number
+  children: ReactNode;
+  width: number;
 }
 
-export {IAppBarProps, ISideNavProps}
+export { IAppBarProps, ISideNavProps };
