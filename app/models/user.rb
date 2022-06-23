ed253daf75dc37d5c12064c7cf1a7d4998955e07
JonYamada <jonathon.yamada@wpengine.com>
2022-06-23 15:00:00 +0100

    made last_name and password mandatory

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :first_name, :email, presence: true
+  validates :first_name, :last_name, :email, :password_digest, presence: true
   validates :email, uniqueness: true
   validate :correct_email_format
 
