b88387021dbb7f40515501d1abd63f6ee9f66c93
JonYamada <jonathon.yamada@wpengine.com>
2022-06-28 13:58:58 +0100

    ensures email present before correct format validation checked

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  
+
   validates :first_name, :last_name, :email, :password_digest, presence: true
   validates :email, uniqueness: true
   validate :correct_email_format
@@ -10,6 +10,6 @@ class User < ApplicationRecord
   private
 
   def correct_email_format
-    errors.add(:email, :invalid_format) if (email =~ /^(.+)@(.+)$/).nil?
+    errors.add(:email, :invalid_format) if (email =~ /^(.+)@(.+)$/).nil? & email.present?
   end
 end
