eef6acfcb035843146019829dea85d10fd1524d4
JonYamada <jonathon.yamada@wpengine.com>
2022-06-28 16:24:09 +0100

    styled authLayout

--- app/javascript/bundles/views/auth/Register.tsx
+++ app/javascript/bundles/views/auth/Register.tsx
@@ -1,6 +1,7 @@
 import React, { ChangeEvent, useState } from "react";
 import {
   buttonText,
+  headings,
   toast as toastTranslations,
 } from "../../config/translations/en.json";
 import { getRoutes } from "../../utils/RoutesConnector";
@@ -16,6 +17,8 @@ import { IWithLoaderProps } from "../articles/interfaces";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import { Input } from "@mui/material";
+import AuthLayout from "../../layouts/AuthLayout";
+import Typography from "@mui/material/Typography";
 
 const defaultProps = {
   loading: false,
@@ -91,102 +94,111 @@ const Register = ({
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={submit} isInitialValid>
-      {() => (
-        <Form>
-          <Card>
-            <CardContent>
-              {[
-                {
-                  id: "first-name",
-                  name: "first_name",
-                  error: !!errors?.first_name?.message,
-                  fullWidth: true,
-                  label: "first name",
-                  onChange: (
-                    event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
-                  ) => {
-                    setValues({
-                      ...values,
-                      first_name:
-                        (event?.target as HTMLInputElement).value || "",
-                    });
+    <AuthLayout>
+      <Typography align="center" variant="h2" sx={{ marginBottom: 3 }}>
+        {headings.signUp}
+      </Typography>
+
+      <Formik initialValues={initialValues} onSubmit={submit} isInitialValid>
+        {() => (
+          <Form>
+            <Card>
+              <CardContent>
+                {[
+                  {
+                    id: "first-name",
+                    name: "first_name",
+                    error: !!errors?.first_name?.message,
+                    fullWidth: true,
+                    label: "first name",
+                    onChange: (
+                      event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+                    ) => {
+                      setValues({
+                        ...values,
+                        first_name:
+                          (event?.target as HTMLInputElement).value || "",
+                      });
+                    },
+                    value: values.first_name,
                   },
-                  value: values.first_name,
-                },
-                {
-                  id: "last-name",
-                  name: "last_name",
-                  error: !!errors?.last_name?.message,
-                  fullWidth: true,
-                  label: "last name",
-                  onChange: (
-                    event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
-                  ) => {
-                    setValues({
-                      ...values,
-                      last_name:
-                        (event?.target as HTMLInputElement).value || "",
-                    });
+                  {
+                    id: "last-name",
+                    name: "last_name",
+                    error: !!errors?.last_name?.message,
+                    fullWidth: true,
+                    label: "last name",
+                    onChange: (
+                      event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+                    ) => {
+                      setValues({
+                        ...values,
+                        last_name:
+                          (event?.target as HTMLInputElement).value || "",
+                      });
+                    },
+                    value: values.last_name,
                   },
-                  value: values.last_name,
-                },
-                {
-                  id: "email",
-                  name: "email",
-                  error: !!errors?.email?.message,
-                  fullWidth: true,
-                  label: "Email",
-                  onChange: (
-                    event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
-                  ) => {
-                    setValues({
-                      ...values,
-                      email: (event?.target as HTMLInputElement).value || "",
-                    });
+                  {
+                    id: "email",
+                    name: "email",
+                    error: !!errors?.email?.message,
+                    fullWidth: true,
+                    label: "email",
+                    onChange: (
+                      event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+                    ) => {
+                      setValues({
+                        ...values,
+                        email: (event?.target as HTMLInputElement).value || "",
+                      });
+                    },
+                    value: values.email,
                   },
-                  value: values.email,
-                },
-                {
-                  id: "password",
-                  name: "password",
-                  error: !!errors?.password?.message,
-                  fullWidth: true,
-                  label: "Password",
-                  onChange: (
-                    event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
-                  ) => {
-                    setValues({
-                      ...values,
-                      password: (event?.target as HTMLInputElement).value || "",
-                    });
+                  {
+                    id: "password",
+                    name: "password",
+                    error: !!errors?.password?.message,
+                    fullWidth: true,
+                    label: "password",
+                    onChange: (
+                      event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+                    ) => {
+                      setValues({
+                        ...values,
+                        password:
+                          (event?.target as HTMLInputElement).value || "",
+                      });
+                    },
+                    type: "password",
+                    value: values.password,
                   },
-                  type: "password",
-                  value: values.password,
-                },
-              ].map(({ label, name, ...rest }) => (
-                <div key={name}>
-                  <label htmlFor={name}>{label}</label>
-                  <Input {...rest} />
-                  {!!errors[name]?.message && (
-                    <div className="error-message">{errors[name].message}</div>
-                  )}
-                </div>
-              ))}
-            </CardContent>
-          </Card>
-          <LoadingButton
-            type="submit"
-            loading={loading}
-            loadingPosition="center"
-            sx={{ mt: 1, float: "right" }}
-            variant="contained"
-          >
-            {buttonText.submit}
-          </LoadingButton>
-        </Form>
-      )}
-    </Formik>
+                ].map(({ label, name, ...rest }) => (
+                  <div key={name}>
+                    <label htmlFor={name}>{label}</label>
+                    <Input {...rest} name={name} id={name} />
+                    {!!errors[name]?.message && (
+                      <div className="error-message">
+                        {errors[name].message}
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </CardContent>
+            </Card>
+            <LoadingButton
+              type="submit"
+              loading={loading}
+              loadingPosition="center"
+              sx={{ mt: 1, float: "right" }}
+              variant="contained"
+            >
+              {buttonText.submit}
+            </LoadingButton>
+          </Form>
+        )}
+      </Formik>
+    </AuthLayout>
   );
 };
 
