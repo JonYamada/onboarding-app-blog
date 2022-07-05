716166210d5619b9bc58a4941e8f9b72b4aab07d
JonYamada <jonathon.yamada@wpengine.com>
2022-07-05 16:34:36 +0100

    added session controller tests

--- spec/controllers/session_spec.rb
+++ spec/controllers/session_spec.rb
@@ -11,12 +11,26 @@ RSpec.describe SessionsController, type: :controller do
   end
 
   describe 'POST /login - Authentication' do
-    it 'successfully authenticates the user' do
+    def create_user
       User.create({ first_name: 'Joe', last_name: 'Bloggs', email: @email, password: @password })
+    end
+
+    def create_session(params)
+      post :create, params: { user: params }
+    end
+
+    it 'successfully authenticates the user' do
+      create_user
       expect(session[:current_user_id]).to be_nil
 
-      post :create, params: { user: { email: @email, password: @password } }
+      create_session({ email: @email, password: @password })
       expect(session[:current_user_id]).to eql(User.find_by_email(@email).id)
+    end
+
+    it 'redirects to root path on login' do
+      create_user
+      create_session({ email: @email, password: @password })
+      expect(response.location).to redirect_to(articles_path)
     end
 
     it 'throws validation error unless both email and password are present' do
@@ -27,10 +41,10 @@ RSpec.describe SessionsController, type: :controller do
       ]
 
       invalid_params.each do |params|
-        post :create, params: { user: params }
+        create_session(params)
         expect(session[:current_user_id]).to be nil
 
-        expect(response.body).to match(/Authentication failed./)
+        expect(JSON.parse(response.body)).to eq({"errors"=>"Authentication failed."})
         expect(response).to have_http_status(422)
         expect(session[:current_user_id]).to be nil
       end
