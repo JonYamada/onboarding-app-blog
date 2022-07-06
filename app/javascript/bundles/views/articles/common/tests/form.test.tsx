72cca22df15a3ff7a09ca92516ce0e0b3293e276
JonYamada <jonathon.yamada@wpengine.com>
2022-07-06 14:47:50 +0100

    removed console errors and prettied form.test.tsx

--- app/javascript/bundles/views/articles/common/tests/form.test.tsx
+++ app/javascript/bundles/views/articles/common/tests/form.test.tsx
@@ -1,112 +1,137 @@
-import React from 'react'
-import '@testing-library/jest-dom'
-import ArticleForm from '../form'
-import {fireEvent, getNodeText, render, screen} from '@testing-library/react'
+import React from "react";
+import "@testing-library/jest-dom";
+import ArticleForm from "../form";
+import {
+  fireEvent,
+  getNodeText,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
 
-describe('Article Form Component', () => {
-  let titleInput: HTMLElement
-  let contentTextarea: HTMLElement
-  let btnSubmit: HTMLElement
+describe("Article Form Component", () => {
+  let titleInput: HTMLElement;
+  let contentTextarea: HTMLElement;
+  let btnSubmit: HTMLElement;
 
-  const populateTitleField = (value: string = 'sample title text') => fireEvent.input(titleInput, {target: {value}})
-  const populateContentField = (value: string = 'sample content text') => fireEvent.input(contentTextarea, {target: {innerHTML: value}})
-  const clickSubmit = () => fireEvent.click(btnSubmit)
+  const populateTitleField = (value: string = "sample title text") => {
+    fireEvent.input(titleInput, { target: { value } });
+  };
+
+  const populateContentField = (value: string = "sample content text") => {
+    fireEvent.input(contentTextarea, { target: { innerHTML: value } });
+  };
+
+  const clickSubmit = async () => {
+    await waitFor(() => {
+      fireEvent.click(btnSubmit);
+    });
+  };
 
   beforeEach(() => {
-    render(<ArticleForm/>)
-    contentTextarea = screen.getByRole('textbox', {name: 'aria-editor'})
-    titleInput = screen.getByLabelText('Title')
-    btnSubmit = screen.getByRole('button', {name: /save/i})
-  })
+    render(<ArticleForm />);
+    titleInput = screen.getByLabelText("Title");
+    contentTextarea = screen.getByRole("textbox", { name: "aria-editor" });
+    btnSubmit = screen.getByRole("button", { name: /save/i });
+  });
 
-  describe('fields', () => {
-    it('expects only 2 textbox fields', () => {
-      const fields = screen.getAllByRole('textbox')
-      expect(fields).toHaveLength(2)
-      fields.forEach(field => expect(field).toBeInTheDocument())
-    })
+  describe("fields", () => {
+    it("expects only 2 textbox fields", () => {
+      const fields = screen.getAllByRole("textbox");
+      expect(fields).toHaveLength(2);
+      fields.forEach((field) => expect(field).toBeInTheDocument());
+    });
 
-    it('allows user to type in title input field', () => {
-      const sampleText = 'My article title'
-      populateTitleField(sampleText)
-      expect(titleInput).toHaveValue(sampleText)
-    })
+    it("allows user to type in title input field", () => {
+      const sampleText = "My article title";
+      populateTitleField(sampleText);
+      expect(titleInput).toHaveValue(sampleText);
+    });
 
-    it('allows user to type in content textarea field', () => {
-      const sampleText = 'My article content'
-      populateContentField(sampleText)
-      expect(getNodeText(contentTextarea)).toBe(sampleText)
-    })
-  })
+    it("allows user to type in content textarea field", () => {
+      const sampleText = "My article content";
+      populateContentField(sampleText);
+      expect(getNodeText(contentTextarea)).toBe(sampleText);
+    });
+  });
 
-  describe('validations', () => {
-    const titleValidationMessage = 'Title Required'
-    const contentValidationMessage = 'Content Required'
+  describe("validations", () => {
+    const titleValidationMessage = "Title Required";
+    const contentValidationMessage = "Content Required";
 
-    const queryTitleValidationMessage = () => screen.queryByText(titleValidationMessage)
-    const queryContentValidationMessage = () => screen.queryByText(contentValidationMessage)
+    const queryTitleValidationMessage = () =>
+      screen.queryByText(titleValidationMessage);
+    const queryContentValidationMessage = () =>
+      screen.queryByText(contentValidationMessage);
 
-    const findTitleValidationMessage = () => screen.findByText(titleValidationMessage)
-    const findContentValidationMessage = () => screen.findByText(contentValidationMessage)
+    const findTitleValidationMessage = () =>
+      screen.findByText(titleValidationMessage);
+    const findContentValidationMessage = () =>
+      screen.findByText(contentValidationMessage);
 
-    const expectTitleValueToBe = (value: string = '') => expect(titleInput).toHaveValue(value)
-    const expectContentValueToBe = (value: string = '') => expect(getNodeText(contentTextarea)).toBe(value)
+    const expectTitleValueToBe = (value: string = "") =>
+      expect(titleInput).toHaveValue(value);
+    const expectContentValueToBe = (value: string = "") =>
+      expect(getNodeText(contentTextarea)).toBe(value);
 
-    it('throws nothing when both title and content are present', async () => {
-      expect(await queryTitleValidationMessage()).not.toBeInTheDocument()
-      expect(await queryContentValidationMessage()).not.toBeInTheDocument()
+    it("throws nothing when both title and content are present", async () => {
+      expect(await queryTitleValidationMessage()).not.toBeInTheDocument();
+      expect(await queryContentValidationMessage()).not.toBeInTheDocument();
 
-      populateTitleField()
-      populateContentField()
-      clickSubmit()
+      populateTitleField();
+      populateContentField();
 
-      expect(await queryTitleValidationMessage()).not.toBeInTheDocument()
-      expect(await queryContentValidationMessage()).not.toBeInTheDocument()
-    })
+      clickSubmit();
 
-    it('is thrown when both title and content fields are absent', async () => {
-      populateTitleField('')
-      populateContentField('')
+      await waitFor(async () => {
+        expect(await queryTitleValidationMessage()).not.toBeInTheDocument();
+        expect(await queryContentValidationMessage()).not.toBeInTheDocument();
+      });
+    });
 
-      expectTitleValueToBe('')
-      expectContentValueToBe('')
+    it("is thrown when both title and content fields are absent", async () => {
+      populateTitleField("");
+      populateContentField("");
 
-      expect(await queryTitleValidationMessage()).not.toBeInTheDocument()
-      expect(await queryContentValidationMessage()).not.toBeInTheDocument()
+      expectTitleValueToBe("");
+      expectContentValueToBe("");
 
-      clickSubmit()
-      expect(await findTitleValidationMessage()).toBeInTheDocument()
-      expect(await findContentValidationMessage()).toBeInTheDocument()
-    })
+      expect(await queryTitleValidationMessage()).not.toBeInTheDocument();
+      expect(await queryContentValidationMessage()).not.toBeInTheDocument();
 
-    it('is thrown when title field present and content field is absent', async () => {
-      populateTitleField()
-      populateContentField('')
+      clickSubmit();
+      expect(await findTitleValidationMessage()).toBeInTheDocument();
+      expect(await findContentValidationMessage()).toBeInTheDocument();
+    });
 
-      expectContentValueToBe('')
-      expect(await queryTitleValidationMessage()).not.toBeInTheDocument()
-      expect(await queryContentValidationMessage()).not.toBeInTheDocument()
+    it("is thrown when title field present and content field is absent", async () => {
+      populateTitleField();
+      populateContentField("");
 
-      clickSubmit()
+      expectContentValueToBe("");
+      expect(await queryTitleValidationMessage()).not.toBeInTheDocument();
+      expect(await queryContentValidationMessage()).not.toBeInTheDocument();
 
-      expect(await queryTitleValidationMessage()).not.toBeInTheDocument()
-      expect(await findContentValidationMessage()).toBeInTheDocument()
-    })
+      clickSubmit();
 
-    it('is thrown when title field absent and content field is present', async () => {
-      populateTitleField('')
-      expectTitleValueToBe('')
+      expect(await queryTitleValidationMessage()).not.toBeInTheDocument();
+      expect(await findContentValidationMessage()).toBeInTheDocument();
+    });
 
-      populateContentField('My Content')
-      expectContentValueToBe('My Content')
+    it("is thrown when title field absent and content field is present", async () => {
+      populateTitleField("");
+      expectTitleValueToBe("");
 
-      expect(await queryTitleValidationMessage()).not.toBeInTheDocument()
-      expect(await queryContentValidationMessage()).not.toBeInTheDocument()
+      populateContentField("My Content");
+      expectContentValueToBe("My Content");
 
-      clickSubmit()
+      expect(await queryTitleValidationMessage()).not.toBeInTheDocument();
+      expect(await queryContentValidationMessage()).not.toBeInTheDocument();
 
-      expect(await queryContentValidationMessage()).not.toBeInTheDocument()
-      expect(await findTitleValidationMessage()).toBeInTheDocument()
-    })
-  })
-})
+      clickSubmit();
+
+      expect(await queryContentValidationMessage()).not.toBeInTheDocument();
+      expect(await findTitleValidationMessage()).toBeInTheDocument();
+    });
+  });
+});
